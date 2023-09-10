dccec600fa1de00d2f741273d3fbb7996601467d
itsspriyansh <priyanshkp01@gmail.com>
2023-09-10 20:01:56 +0530

    protecting route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,22 @@
+import { getSession } from "next-auth/react";
+
+export async function getServerSideProps (context : NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect : {
+        destination : "/Auth",
+        permanent : false
+      }
+    }
+  }
+
+  return {
+    props : {}
+  }
+}
+
 export default function Home() {
   return (
     <div>
