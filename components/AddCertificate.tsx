56b013319ab9b4e2cbb5001db211af8cbdbefb32
Shreya Singh <singh.shreya0511@gmail.com>
2023-09-11 15:53:24 +0530

    Added Drag and Drop

--- components/AddCertificate.tsx
+++ components/AddCertificate.tsx
@@ -29,9 +29,9 @@ const AddCertificate: React.FC<Props> = () => {
   return (
     <>
     {currentAccount}
-      <input type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
-      <input type='text' placeholder='Department' value={dept} onChange={(e) => setDept(e.target.value)} />
-      <input type='text' placeholder='ImgUrl' value={img} onChange={(e) => setImg(e.target.value)} />
+      <input type='text' placeholder='Name' value={name} onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setName(e.target.value)} />
+      <input type='text' placeholder='Department' value={dept} onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setDept(e.target.value)} />
+      <input type='text' placeholder='ImgUrl' value={img} onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setImg(e.target.value)} />
       <button onClick={addCertificate}>Add</button>
     </>
   );
--- pages/Add.tsx
+++ pages/Add.tsx
@@ -1,10 +1,12 @@
 import AddCertificate from '@/components/AddCertificate';
+import DragAndDrop from '@/components/DragAndDrop';
 import React from 'react';
 
 const Add: React.FC = () => {
   return (
     <div>
     <AddCertificate/>
+    <DragAndDrop/>
     </div>
   );
 };
